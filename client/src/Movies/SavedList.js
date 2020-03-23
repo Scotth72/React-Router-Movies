02ddc8b921308a45bb425ab8ba53aeedae3e6e2e
Heath Scott <HeathScott@Heaths-MacBook-Pro.local>
2020-03-23 17:20:13 -0500

    Completed for MVP.

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
-const SavedList = props => (
+const SavedList = props =>{
+  let history = useHistory();
+  return(
+
   <div className="saved-list">
     <h3>Saved Movies:</h3>
     {props.list.map(movie => (
       <span className="saved-movie">{movie.title}</span>
     ))}
-    <div className="home-button">Home</div>
+    
+    <button className="home-button" onClick={() => history.goBack()}>Home</button>
   </div>
-);
+
+)};
 
 export default SavedList;
